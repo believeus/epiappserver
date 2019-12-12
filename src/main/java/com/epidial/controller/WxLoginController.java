00428ca03e07d499905c4e6420cc10e84e47b5be
3DA1B4E93212B415C9F52103915948A8 <3DA1B4E93212B415C9F52103915948A8@i-search.com.cn>
2019-12-12 14:30:47 +0800

    island

--- src/main/java/com/epidial/controller/WxLoginController.java
+++ src/main/java/com/epidial/controller/WxLoginController.java
@@ -24,9 +24,8 @@ public class WxLoginController {
     @Resource
     private QuestionnaireDao questionnaireDao;
 
-    @ResponseBody
     @RequestMapping("/user/wx/login")
-    public ModelAndView wxlogin(HttpServletRequest request,HttpServletResponse response) throws Exception {
+    public ModelAndView wxlogin(HttpServletRequest request) throws Exception {
         try {
             String openid = request.getParameter("openid");
             User user = userDao.findUser("uuid", openid);
@@ -41,23 +40,23 @@ public class WxLoginController {
                 ClassLoader classLoader = this.getClass().getClassLoader();
                 FileInputStream in = new FileInputStream(classLoader.getResource("application.properties").getPath());
                 properties.load(in);
-                String classpath = classLoader.getResource("/").getPath().substring(1);
+                String classpath = classLoader.getResource("/").getPath();
                 String html = FileUtils.readFileToString(new File(classpath + "dachang.html"),"UTF-8");
                 Questionnaire data = new Questionnaire();
                 data.setUuid(openid);
-                data.setComtab(html);
+                data.setComtab("html");
                 //将字符串保存到
                 questionnaireDao.save(data);
                 ModelAndView modelAndView = new ModelAndView();
-                modelAndView.setViewName("/WEB-INF/front/quse2.jsp");
+                modelAndView.setViewName("/WEB-INF/front/ques2.jsp");
                 return modelAndView;
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
         ModelAndView modelAndView = new ModelAndView();
-        modelAndView.setViewName("/WEB-INF/front/quse2.jsp");
+        modelAndView.setViewName("/WEB-INF/front/ques2.jsp");
         return modelAndView;
-        
+
     }
 }
--- src/main/webapp/WEB-INF/front/ques2.jsp
+++ src/main/webapp/WEB-INF/front/ques2.jsp
@@ -1,20 +1,22 @@
+<%@ taglib prefix="form" uri="http://www.springframework.org/tags/form" %>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <%
     String path = request.getContextPath();
     String basePath = request.getScheme() + "://" + request.getServerName() + ":" + request.getServerPort() + path + "/";
 %>
+<!DOCTYPE html>
 <html>
 <head>
-    <title>早期防治知识调查问卷</title>
+    <title>北京肛肠学会</title>
     <base href="<%=basePath%>">
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
     <meta name="viewport" content="width=device-width,minimum-scale=1,initial-scale=1,shrink-to-fit=no">
-    
 </head>
-    <body>
-        <div style="width: 100%;height: 600px">
-            <img src="static/images/indextxt.jpg" style="width: 100%;height: auto">
-        </div>
-    </body>
+<body>
+<div style="width: 100%;height: 600px">
+        <img src="/static/images/indextxt.jpg" style="width: 100%;height: auto">
+</div>
+
+</body>
 </html>
