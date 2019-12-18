c16f11bd53cb3870a97aacc14878d10917fb374d
3DA1B4E93212B415C9F52103915948A8 <3DA1B4E93212B415C9F52103915948A8@i-search.com.cn>
2019-12-18 17:20:05 +0800

    island

---
+++ src/main/java/com/epidial/controller/TestController.java
@@ -0,0 +1,78 @@
+package com.epidial.controller;
+
+import com.alibaba.fastjson.JSONObject;
+import com.epidial.utils.AesUtil;
+import com.epidial.utils.HttpRequest;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+
+public class TestController {
+
+
+    @ResponseBody
+    @RequestMapping(value = "/user/getopenid", method = RequestMethod.GET)
+    public Map getOpenid(String code, String encryptedData, String iv ){
+        Map<String,Object> map = new HashMap<String,Object>();
+
+        //登录凭证不能为空
+        if (code == null || code.length() == 0) {
+            map.put("status", 0);
+            map.put("msg", "code 不能为空");
+            System.out.println("map1:" + map);
+            return map;
+        }
+        //小程序唯一标识   (在微信小程序管理后台获取)
+        String wxspAppid = "wx4f53ae05242a3e7b";
+        //小程序的 app secret (在微信小程序管理后台获取)
+        String wxspSecret = "73acec450c056332720f026523e89e64";
+        //授权（必填）
+        String grant_type = "authorization_code";
+        //////////////// 1、向微信服务器 使用登录凭证 code 获取 session_key 和 openid ////////////////
+        //请求参数
+        String params = "appid=" + wxspAppid + "&secret=" + wxspSecret + "&js_code=" + code + "&grant_type=" + grant_type;
+        //发送请求
+        String sr = HttpRequest.sendGet("https://api.weixin.qq.com/sns/jscode2session", params);
+        //解析相应内容（转换成json对象）
+        JSONObject json = JSONObject.parseObject(sr);
+        //获取会话密钥（session_key）
+        String session_key = json.get("session_key").toString();
+        //用户的唯一标识（openid）
+        String openid = (String) json.get("openid");
+        System.out.println("openid:" + openid);
+        //////////////// 2、对encryptedData加密数据进行AES解密 ////////////////
+        try {
+            String result = AesUtil.decrypt(encryptedData, session_key, iv, "UTF-8");
+            if (null != result && result.length() > 0) {
+                map.put("status", 1);
+                map.put("msg", "解密成功");
+
+                JSONObject userInfoJSON = JSONObject.parseObject(result);
+                Map<String,Object> userInfo = new HashMap<String,Object>();
+                userInfo.put("openId", userInfoJSON.get("openId"));
+                userInfo.put("nickName", userInfoJSON.get("nickName"));
+                userInfo.put("gender", userInfoJSON.get("gender"));
+                userInfo.put("city", userInfoJSON.get("city"));
+                userInfo.put("province", userInfoJSON.get("province"));
+                userInfo.put("country", userInfoJSON.get("country"));
+                userInfo.put("avatarUrl", userInfoJSON.get("avatarUrl"));
+                userInfo.put("unionId", userInfoJSON.get("unionId"));
+                map.put("userInfo", userInfo);
+                System.out.println("map2:" + map);
+                return map;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        map.put("status", 0);
+        map.put("msg", "解密失败");
+        System.out.println("map3:" + map);
+        return map;
+    }
+}
