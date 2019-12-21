3e7cae66323a15fd9a1a868666587e799a2550fe
3DA1B4E93212B415C9F52103915948A8 <3DA1B4E93212B415C9F52103915948A8@i-search.com.cn>
2019-12-21 12:41:36 +0800

    island

--- src/main/java/com/epidial/controller/WxQuesController.java
+++ src/main/java/com/epidial/controller/WxQuesController.java
@@ -44,7 +44,7 @@ public class WxQuesController {
         //解析相应内容（转换成json对象）
         JSONObject json = JSONObject.parseObject(sr);
         //获取会话密钥（session_key）
-        String session_key = json.get("session_key").toString();
+//        String session_key = json.get("session_key").toString();
         //用户的唯一标识（openid）
         String openid = (String) json.get("openid");
         System.out.println("openid:" + openid);
