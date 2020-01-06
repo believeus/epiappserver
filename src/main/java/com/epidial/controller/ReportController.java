e0a9fac54b06f1a0eb717daa696c2c22e8725820
wuqiwei <1058633117@qq.com>
2020-01-06 10:32:09 +0800

    modify

--- src/main/java/com/epidial/controller/ReportController.java
+++ src/main/java/com/epidial/controller/ReportController.java
@@ -138,13 +138,22 @@ public class ReportController {
             ClassLoader classLoader = this.getClass().getClassLoader();
             FileInputStream in = new FileInputStream(classLoader.getResource("application.properties").getPath());
             properties.load(in);
-            String classpath = classLoader.getResource("/").getPath().substring(1);
-            String phantomjs = classpath + "window-x64-phantomjs.exe";
+           String classpath = classLoader.getResource("/").getPath().substring(1);
+           //Window
+/*            String phantomjs = classpath + "window-x64-phantomjs.exe";
             String rasterize = classpath + "rasterize.js";
             String url = properties.getProperty("host") + "user/report/" + uuid + "/" + barcode + "/"+locale+"/dnaview.jhtml";
             String filename = properties.getProperty("pdfpath") + "biological-age-barcode-" + barcode + "-"+locale+".pdf";
-            String cmd = "cmd /c " + phantomjs + " " + rasterize + " " + url + " " + filename;
+            String cmd = "cmd /c " + phantomjs + " " + rasterize + " " + url + " " + filename;*/
+
+            //linux
+            String phantomjs = "phantomjs"; //phantonjs已经配置好环境变量
+            String rasterize = classpath + "rasterize.js";
+            String filename = properties.getProperty("pdfpath") + "biological-age-barcode-" + barcode + "-"+locale+".pdf";
+            String url = properties.getProperty("host") + "user/report/" + uuid + "/" + barcode + "/"+locale+"/dnaview.jhtml";
+            String cmd = phantomjs + " " + rasterize + " " + url + " " + filename;
             System.out.println(cmd);
+
             Process proc = Runtime.getRuntime().exec(cmd);
             int exitVal = proc.waitFor(); // 阻塞当前线程，并等待外部程序中止后获取结果码
             in.close();
@@ -164,15 +173,18 @@ public class ReportController {
             properties.load(in);
             response.setHeader("Content-Disposition", "attachment;fileName=" + URLEncoder.encode("biological-age-barcode-" + barcode + "-"+locale+".pdf", "UTF-8"));
             String filename = properties.getProperty("pdfpath") + "biological-age-barcode-" + barcode + "-"+locale+".pdf";
-            InputStream fileIn = new FileInputStream(filename);
-            OutputStream fileOut = response.getOutputStream();
-            byte[] buff = new byte[1024];
-            int len = 0;
-            while ((len = fileIn.read(buff)) != -1) {
-                fileOut.write(buff, 0, len);
+            File file=new File(filename);
+            if (file.exists()) {
+                InputStream fileIn = new FileInputStream(filename);
+                OutputStream fileOut = response.getOutputStream();
+                byte[] buff = new byte[1024];
+                int len = 0;
+                while ((len = fileIn.read(buff)) != -1) {
+                    fileOut.write(buff, 0, len);
+                }
+                fileOut.close();
+                fileIn.close();
             }
-            fileOut.close();
-            fileIn.close();
             in.close();
         } catch (Exception e) {
             e.printStackTrace();
